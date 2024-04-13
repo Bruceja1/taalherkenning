8736849c98ad9211db59e3c2831d7d9849cd814f
Bruceja <2103932jagessar@zuyd.nl>
2024-04-13 14:14:54 +0200

    Bi- en trigrammen invoerzinnen

--- taalherkenning.py
+++ taalherkenning.py
@@ -2,6 +2,7 @@
 # Engels, Malayalam, Hindi, Tamil, Kannada, Frans, Spaans, Portugees, Italiaans, Russisch, Zweeds, Nederlands,
 # Arabisch, Turks, Duits, Deens, Grieks
 import pandas as pd
+import re
 from collections import defaultdict
 
 df = pd.read_csv('languages.csv', sep = ',')
@@ -9,7 +10,7 @@ df = pd.read_csv('languages.csv', sep = ',')
 df['Text'] = df['Text'].str.lower()
 df['Text'] = df['Text'].str.strip()
 
-symbols = [",", ".", "?", "(", ")", '"', "'", "[", "]", ":", ";", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!",",", ".", "?", "(", ")", '"', "'", "[", "]", ":", ";", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 for s in symbols:
     df['Text'] = df['Text'].str.replace(s,'')
 print(df.head(100))
@@ -56,9 +57,38 @@ def get_bigram_frequencies(df):
             bigram_frequencies[language][bigram] = count / total_bigrams
 
     return bigram_frequencies
-    
+
+def get_language_probabilities(userInput):
+    inputTrigrams = []
+    for i in range(len(userInput) - 2):
+        trigram = userInput[i:i + 3]
+        inputTrigrams.append(trigram)
+    print(f"De trigrammen van de input zijn: {inputTrigrams}")
+
+    inputBigrams = []
+    for i in range(1, len(userInput) - 2):
+        bigram = userInput[i:i + 2]
+        inputBigrams.append(bigram)
+    print(f"De 'tussen' bigrammen van de input zijn: {inputBigrams}")
+
 
 trigram_frequencies = get_trigram_frequencies(df)
 bigram_frequencies = get_bigram_frequencies(df)
 
-print(bigram_frequencies['Dutch'])
+""" for language in bigram_frequencies.keys():
+    print(f"Taal: {language} heeft als som: {sum(bigram_frequencies[language].values())}") """
+
+while True:
+    userInput = input("Geef een voorbeeldzin... \n")
+    m = re.compile(r'[a-zA-Z0-9().?!]')
+    if (m.match(userInput)):
+        break
+    else:
+        print("De invoer mag alleen de volgende tekens bevatten: 'a-zA-Z0-9().?!'")
+
+for s in symbols:
+    userInput = userInput.replace(s, '')
+userInput = userInput.lower()
+userInput = userInput.strip()
+print(f"De nieuwe input is: {userInput}")
+get_language_probabilities(userInput)
